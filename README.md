68d418aef575c50918af337d23e81dbe5bf1d975
Jacopo Jannone <jacopo-j@users.noreply.github.com>
2017-09-03 00:06:48 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Introduction
+
+This is a Python 3 module for controlling the so called "MCR12" barcode scanner module via USB serial port emulation. The MCR12 is sold as standalone module, [for example on Adafruit](https://www.adafruit.com/product/1202), and it is also the module on which some barcode scanner, [such as this one](https://www.amazon.com/dp/B01FVYVX5A), are based.
+
+
+## Usage
+
+```python
+from mcr12_serial import BarcodeScanner
+
+bcd = BarcodeScanner("/path/to/serial/device", baudrate=9600)
+
+barcode1 = bcd.scan()  # Same as bcd.scan(0)
+'''The scanner will start scanning indefinitely until a barcode is
+scanned.'''
+
+barcode2 = bcd.scan(5000)
+'''The scanner will start scanning. If after 5000 milliseconds no
+barcode has been scanned, a TimeoutError is raised.'''
+
+bcd.stop()
+'''Stop scanning. Not really useful here as the scanner stops scanning
+automatically after a barcode has been scanned or after the timeout has
+expired. However it may be useful if you call the scan() method
+asynchronously or if you need to handle a KeyboardInterrupt to stop
+scanning after calling the scan() method.'''
+
+bcd.config("013300")
+'''Edit the scanner configuration. "013300" must be replaced with a
+configuration code.'''
+```
+
+### Configuration codes
+
+You can find configuration codes on the [MCR 12 Manual](https://cdn-shop.adafruit.com/product-files/1203/MCR12_Scanner_Manual.pdf). They are written under each configuration barcode.
---
+++ mcr12_serial.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import serial
+
+class BarcodeScanner(serial.Serial):
+    NUL = STOP_SCANNING = 0x00
+    CMD = SCANNER_CTRL = SCAN_FOREVER = 0x01
+    STX = SET = SCAN_TIMEOUT = 0x02
+    ETX = SAVE_CONF = 0x03
+    CONFIG_FLAG = 0x82
+    HFF = 0xFF
+    HAA = 0xAA
+    H16 = 0x100
+    H85 = 0x55
+    SAVE_CMD = [CMD, SAVE_CONF, HAA, H85]
+
+    TIMEOUT_VALUE_ERR = "timeout must be between 0 and 65535 milliseconds"
+    TIMEOUT_TYPE_ERR = "timeout must be an integer, not '{}'"
+    CONFIG_TYPE_ERR = "configuration code must a string, not '{}'"
+    CONFIG_LEN_ERR = "length of the configuration code must be between 1 and 10 characters"
+    SCAN_TIMED_OUT = "operation timed out."
+
+    def __init__(self, dev, baudrate=9600, timeout=None):
+        super().__init__(dev, baudrate, 8, serial.PARITY_NONE, timeout=timeout)
+
+    def _checksum(self, data_bytes):
+        # Input: from byte 1 to byte 13 (index 0).
+        byte_sum = self.STX + self.ETX + sum(data_bytes)
+        return self.H16 - (byte_sum & self.HFF)
+
+    def _build_bin_str(self, data_bytes):
+        bin_data = [self.STX] + data_bytes + [self.NUL] * (13 - len(data_bytes))
+        bin_data += [self.ETX, self._checksum(data_bytes)]
+        return bytes(bin_data)
+
+    def _send_command(self, command):
+        self.flushInput()
+        self.flushOutput()
+        self.write(command)
+        self.flush()
+
+    def scan(self, timeout=0):
+        # Timeout in milliseconds. 0 = no timeout.
+        if not isinstance(timeout, int):
+            raise TypeError(self.TIMEOUT_TYPE_ERR.format(type(timeout).__name__))
+        if (timeout > 65535) or (timeout < 0):
+            raise ValueError(self.TIMEOUT_VALUE_ERR)
+        data_bytes = [self.CMD, self.SCANNER_CTRL]
+        if (timeout == 0):
+            data_bytes.append(self.SCAN_FOREVER)
+            self.timeout = None
+        else:
+            high_tx, low_tx = divmod(timeout, self.H16)
+            data_bytes.append(self.SCAN_TIMEOUT)
+            data_bytes.append(low_tx)
+            data_bytes.append(high_tx)
+            self.timeout = timeout / 1000
+        self._send_command(self._build_bin_str(data_bytes))
+        scan_data = self.readline()
+        if (len(scan_data) == 0):
+            raise TimeoutError(self.SCAN_TIMED_OUT)
+        return scan_data.decode().strip()
+
+    def stop(self):
+        data_bytes = [self.CMD, self.SCANNER_CTRL, self. STOP_SCANNING]
+        self._send_command(self._build_bin_str(data_bytes))
+
+    def config(self, conf_code):
+        if not isinstance(conf_code, str):
+            raise TypeError(self.CONFIG_TYPE_ERR.format(type(conf_code).__name__))
+        conf_bytes = []
+        for char in conf_code:
+            conf_bytes.append(ord(char))
+        if (len(conf_bytes) > 10) or (len(conf_bytes) == 0):
+            raise ValueError(self.CONFIG_LEN_ERR)
+        data_bytes = [self.SET, len(conf_bytes) + 1, self.CONFIG_FLAG]
+        data_bytes += conf_bytes
+        self._send_command(self._build_bin_str(data_bytes))
+        self._send_command(self._build_bin_str(self.SAVE_CMD))
+
+
+def main():
+    bcd = BarcodeScanner("/dev/cu.usbmodem1421")
+    bcd.scan(2000)
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
